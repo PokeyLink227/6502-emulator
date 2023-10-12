78c9cdf2cbb99cdb4fd5d25b80f95c91d1a8fe46
Thomas Rice <thomasrice7@yahoo.com>
2023-10-12 00:11:00 -0400

    added some opcodes

--- 6502.c
+++ 6502.c
@@ -3,34 +3,47 @@
 
 typedef unsigned char byte;
 
-struct instruction {
+typedef struct instruction {
     byte opcode, addr_mode, cycles;
 } Instr;
 
-Instr opcode_lookup[0xF][0xF] = {
-    {{OP_BRK, ADR_IMPL  , 7}, {OP_ORA, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ZP    , 3}, {OP_ASL, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PHP, ADR_IMPL  , 3}, {OP_ORA, ADR_IMM   , 2}, {OP_ASL, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ABS   , 4}, {OP_ASL, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BPL, ADR_REL   , 2}, {OP_ORA, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ZPX   , 4}, {OP_ASL, ADR_APX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_CLC, ADR_IMPL  , 2}, {OP_ORA, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ABSX  , 4}, {OP_ASL, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_JSR, ADR_ABS   , 6}, {OP_AND, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_BIT, ADR_ZP    , 3}, {OP_AND, ADR_ZP    , 3}, {OP_ROL, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PLP, ADR_IMPL  , 4}, {OP_AND, ADR_IMM   , 2}, {OP_ROL, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_BIT, ADR_ABS   , 4}, {OP_AND, ADR_ABS   , 4}, {OP_ROL, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BMI, ADR_REL   , 2}, {OP_AND, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_AND, ADR_ZPX   , 4}, {OP_ROL, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_SEC, ADR_IMPL  , 2}, {OP_AND, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_AND, ADR_ABSX  , 4}, {OP_ROL, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_RTI, ADR_IMPL  , 6}, {OP_EOR, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_EOR, ADR_ZP    , 3}, {OP_LSR, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PHA, ADR_IMPL  , 3}, {OP_EOR, ADR_IMM   , 2}, {OP_LSR, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_JMP, ADR_ABS   , 3}, {OP_EOR, ADR_ABS   , 4}, {OP_LSR, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BVC, ADR_REL   , 2}, {OP_EOR, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_EOR, ADR_ZPX   , 4}, {OP_LSR, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_CLI, ADR_IMPL  , 2}, {OP_EOR, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_EOR, ADR_ABSX  , 4}, {OP_LSR, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_RTS, ADR_IMPL  , 6}, {OP_ADC, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ADC, ADR_ZP    , 3}, {OP_ROR, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PLA, ADR_IMPL  , 4}, {OP_ADC, ADR_IMM   , 2}, {OP_ROR, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_JMP, ADR_INDABS, 5}, {OP_ADC, ADR_ABS   , 4}, {OP_ROR, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BVS, ADR_REL   , 2}, {OP_ADC, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ADC, ADR_ZPX   , 4}, {OP_ROR, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_SEI, ADR_IMPL  , 2}, {OP_ADC, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ADC, ADR_ABSX  , 4}, {OP_ROR, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_XXX, ADR_NONE  , 1}, {OP_STA, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_STY, ADR_ZP    , 3}, {OP_STA, ADR_ZP    , 3}, {OP_STX, ADR_ZP    , 3}, {OP_XXX, ADR_NONE  , 1}, {OP_DEY, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_TXA, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_STY, ADR_ABS   , 4}, {OP_STA, ADR_ABS   , 4}, {OP_STX, ADR_ABS   , 4}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BCC, ADR_REL   , 2}, {OP_STA, ADR_INDY  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_STY, ADR_ZPX   , 4}, {OP_STA, ADR_ZPX   , 4}, {OP_STX, ADR_ZPX   , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_TYA, ADR_IMPL  , 2}, {OP_STA, ADR_ABSY  , 5}, {OP_TXS, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_STA, ADR_ABSX  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_LDY, ADR_IMM   , 2}, {OP_LDA, ADR_INDX  , 6}, {OP_LDX, ADR_IMM   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ZP    , 3}, {OP_LDA, ADR_ZP    , 3}, {OP_LDX, ADR_ZP    , 3}, {OP_XXX, ADR_NONE  , 1}, {OP_TAY, ADR_IMPL  , 2}, {OP_LDA, ADR_IMM   , 2}, {OP_TAX, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ABS   , 4}, {OP_LDA, ADR_ABS   , 4}, {OP_LDX, ADR_ABS   , 4}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BCS, ADR_REL   , 2}, {OP_LDA, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ZPX   , 3}, {OP_LDA, ADR_ZPX   , 4}, {OP_LDX, ADR_ZPX   , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_CLV, ADR_IMPL  , 2}, {OP_LDA, ADR_ABSY  , 4}, {OP_TSX, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ABSX  , 4}, {OP_LDA, ADR_ABSX  , 4}, {OP_ADC, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_CPY, ADR_IMM   , 2}, {OP_CMP, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CPY, ADR_ZP    , 3}, {OP_CMP, ADR_ZP    , 3}, {OP_DEC, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_INY, ADR_IMPL  , 2}, {OP_CMP, ADR_IMM   , 2}, {OP_DEX, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_CPY, ADR_ABS   , 4}, {OP_CMP, ADR_ABS   , 4}, {OP_DEC, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BNE, ADR_REL   , 2}, {OP_CMP, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CMP, ADR_ZPX   , 4}, {OP_DEC, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_CLD, ADR_IMPL  , 2}, {OP_CMP, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CMP, ADR_ABSX  , 4}, {OP_DEC, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_CPX, ADR_IMM   , 2}, {OP_SBC, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CPX, ADR_ZP    , 3}, {OP_SBC, ADR_ZP    , 3}, {OP_INC, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_INX, ADR_IMPL  , 2}, {OP_SBC, ADR_IMM   , 2}, {OP_NOP, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_CPX, ADR_ABS   , 4}, {OP_SBC, ADR_ABS   , 4}, {OP_INC, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1}},
-    {{OP_BEQ, ADR_REL   , 2}, {OP_SBC, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_SBC, ADR_ZPX   , 4}, {OP_INC, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_SED, ADR_IMPL  , 2}, {OP_SBC, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_SBC, ADR_ABSX  , 4}, {OP_INC, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1}}
+Instr opcode_lookup[0x100] = {
+    {OP_BRK, ADR_IMPL  , 7}, {OP_ORA, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ZP    , 3}, {OP_ASL, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PHP, ADR_IMPL  , 3}, {OP_ORA, ADR_IMM   , 2}, {OP_ASL, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ABS   , 4}, {OP_ASL, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BPL, ADR_REL   , 2}, {OP_ORA, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ZPX   , 4}, {OP_ASL, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_CLC, ADR_IMPL  , 2}, {OP_ORA, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ORA, ADR_ABSX  , 4}, {OP_ASL, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1},
+    {OP_JSR, ADR_ABS   , 6}, {OP_AND, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_BIT, ADR_ZP    , 3}, {OP_AND, ADR_ZP    , 3}, {OP_ROL, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PLP, ADR_IMPL  , 4}, {OP_AND, ADR_IMM   , 2}, {OP_ROL, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_BIT, ADR_ABS   , 4}, {OP_AND, ADR_ABS   , 4}, {OP_ROL, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BMI, ADR_REL   , 2}, {OP_AND, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_AND, ADR_ZPX   , 4}, {OP_ROL, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_SEC, ADR_IMPL  , 2}, {OP_AND, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_AND, ADR_ABSX  , 4}, {OP_ROL, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1},
+    {OP_RTI, ADR_IMPL  , 6}, {OP_EOR, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_EOR, ADR_ZP    , 3}, {OP_LSR, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PHA, ADR_IMPL  , 3}, {OP_EOR, ADR_IMM   , 2}, {OP_LSR, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_JMP, ADR_ABS   , 3}, {OP_EOR, ADR_ABS   , 4}, {OP_LSR, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BVC, ADR_REL   , 2}, {OP_EOR, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_EOR, ADR_ZPX   , 4}, {OP_LSR, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_CLI, ADR_IMPL  , 2}, {OP_EOR, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_EOR, ADR_ABSX  , 4}, {OP_LSR, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1},
+    {OP_RTS, ADR_IMPL  , 6}, {OP_ADC, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ADC, ADR_ZP    , 3}, {OP_ROR, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_PLA, ADR_IMPL  , 4}, {OP_ADC, ADR_IMM   , 2}, {OP_ROR, ADR_ACC   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_JMP, ADR_INDABS, 5}, {OP_ADC, ADR_ABS   , 4}, {OP_ROR, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BVS, ADR_REL   , 2}, {OP_ADC, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ADC, ADR_ZPX   , 4}, {OP_ROR, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_SEI, ADR_IMPL  , 2}, {OP_ADC, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_ADC, ADR_ABSX  , 4}, {OP_ROR, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1},
+    {OP_XXX, ADR_NONE  , 1}, {OP_STA, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_STY, ADR_ZP    , 3}, {OP_STA, ADR_ZP    , 3}, {OP_STX, ADR_ZP    , 3}, {OP_XXX, ADR_NONE  , 1}, {OP_DEY, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_TXA, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_STY, ADR_ABS   , 4}, {OP_STA, ADR_ABS   , 4}, {OP_STX, ADR_ABS   , 4}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BCC, ADR_REL   , 2}, {OP_STA, ADR_INDY  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_STY, ADR_ZPX   , 4}, {OP_STA, ADR_ZPX   , 4}, {OP_STX, ADR_ZPX   , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_TYA, ADR_IMPL  , 2}, {OP_STA, ADR_ABSY  , 5}, {OP_TXS, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_STA, ADR_ABSX  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1},
+    {OP_LDY, ADR_IMM   , 2}, {OP_LDA, ADR_INDX  , 6}, {OP_LDX, ADR_IMM   , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ZP    , 3}, {OP_LDA, ADR_ZP    , 3}, {OP_LDX, ADR_ZP    , 3}, {OP_XXX, ADR_NONE  , 1}, {OP_TAY, ADR_IMPL  , 2}, {OP_LDA, ADR_IMM   , 2}, {OP_TAX, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ABS   , 4}, {OP_LDA, ADR_ABS   , 4}, {OP_LDX, ADR_ABS   , 4}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BCS, ADR_REL   , 2}, {OP_LDA, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ZPX   , 3}, {OP_LDA, ADR_ZPX   , 4}, {OP_LDX, ADR_ZPX   , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_CLV, ADR_IMPL  , 2}, {OP_LDA, ADR_ABSY  , 4}, {OP_TSX, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_LDY, ADR_ABSX  , 4}, {OP_LDA, ADR_ABSX  , 4}, {OP_ADC, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1},
+    {OP_CPY, ADR_IMM   , 2}, {OP_CMP, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CPY, ADR_ZP    , 3}, {OP_CMP, ADR_ZP    , 3}, {OP_DEC, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_INY, ADR_IMPL  , 2}, {OP_CMP, ADR_IMM   , 2}, {OP_DEX, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_CPY, ADR_ABS   , 4}, {OP_CMP, ADR_ABS   , 4}, {OP_DEC, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BNE, ADR_REL   , 2}, {OP_CMP, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CMP, ADR_ZPX   , 4}, {OP_DEC, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_CLD, ADR_IMPL  , 2}, {OP_CMP, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CMP, ADR_ABSX  , 4}, {OP_DEC, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1},
+    {OP_CPX, ADR_IMM   , 2}, {OP_SBC, ADR_INDX  , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_CPX, ADR_ZP    , 3}, {OP_SBC, ADR_ZP    , 3}, {OP_INC, ADR_ZP    , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_INX, ADR_IMPL  , 2}, {OP_SBC, ADR_IMM   , 2}, {OP_NOP, ADR_IMPL  , 2}, {OP_XXX, ADR_NONE  , 1}, {OP_CPX, ADR_ABS   , 4}, {OP_SBC, ADR_ABS   , 4}, {OP_INC, ADR_ABS   , 6}, {OP_XXX, ADR_NONE  , 1},
+    {OP_BEQ, ADR_REL   , 2}, {OP_SBC, ADR_INDY  , 5}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_SBC, ADR_ZPX   , 4}, {OP_INC, ADR_ZPX   , 6}, {OP_XXX, ADR_NONE  , 1}, {OP_SED, ADR_IMPL  , 2}, {OP_SBC, ADR_ABSY  , 4}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_XXX, ADR_NONE  , 1}, {OP_SBC, ADR_ABSX  , 4}, {OP_INC, ADR_ABSX  , 7}, {OP_XXX, ADR_NONE  , 1}
 };
 
 unsigned short pc;
 byte acc, x, y, stkptr, status;
-byte memory[0xFFFF];
+//byte memory[0xFFFF];
 
 Instr cur_instr;
+
+void set_flag(byte flag, byte b) {
+    if (b) {
+        status |= flag;
+    } else {
+        status &= ~flag;
+    }
+    return;
+}
+
+byte get_flag(byte flag) {
+    return status & flag;
+}
 
 byte set_address_mode(byte mode) {
     return 0; // num extra cycles
@@ -53,7 +66,7 @@ byte execute_instr(byte instr) {
     case OP_BCC: {
         return 0;
     }
-    
+
     case OP_BCS: {
         return 0;
     }
@@ -240,32 +253,49 @@ byte execute_instr(byte instr) {
 
     case OP_TAX: {
         x = acc;
+        set_flag(ZERO, x == 0x00);
+        set_flag(NEGATIVE, x & 0x80);
         pc++;
         return 0;
     }
 
     case OP_TAY: {
         y = acc;
+        set_flag(ZERO, y == 0x00);
+        set_flag(NEGATIVE, y & 0x80);
         pc++;
         return 0;
     }
 
     case OP_TSX: {
+        x = stkptr;
+        set_flag(ZERO, x == 0x00);
+        set_flag(NEGATIVE, x & 0x80);
+        pc++;
         return 0;
     }
 
     case OP_TXA: {
+        acc = x;
+        set_flag(ZERO, acc == 0x00);
+        set_flag(NEGATIVE, acc & 0x80);
+        pc++;
         return 0;
     }
 
     case OP_TXS: {
+        stkptr = x;
         return 0;
     }
 
     case OP_TYA: {
+        acc = y;
+        set_flag(ZERO, acc == 0x00);
+        set_flag(NEGATIVE, acc & 0x80);
+        pc++;
         return 0;
     }
-
+    }
     return 0;
 }
 
@@ -292,7 +322,8 @@ int main(int argc, char **argv) {
     pc = 0;
     stkptr = 0;
     status = UNUSED;
-    cur_instr = {0, 0, 0};
+    cur_instr = opcode_lookup[0xBA];
+    printf("%s\n", mnemonics[cur_instr.opcode]);
 
     reset_cpu();
 
