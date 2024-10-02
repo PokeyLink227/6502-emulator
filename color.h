c1c57f3fd9bc500d5495d217d026c165f1e29de3
PokeyLink227 <thomasrice7@yahoo.com>
2024-10-02 14:01:39 -0400

    started adding support for unicode

--- color.h
+++ color.h
@@ -3,19 +3,32 @@
 
 void init_color();
 
-#if defined(_WIN32)
-    #include <windows.h>
+#define HorizontalBar "─"
+#define VerticalBar "│"
+#define DownRight "┌"
+#define DownLeft "┐"
+#define UpRight "┐"
+#define UpLeft "┘"
+#define TRight "├"
+#define TLeft "┤"
+#define TDown "┬"
+#define TUp "┴"
 
-    void init_color() {
-        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-        DWORD dwMode = 0;
-        GetConsoleMode(hStdout, &dwMode);
-        SetConsoleMode(hStdout, dwMode|ENABLE_VIRTUAL_TERMINAL_PROCESSING|ENABLE_PROCESSED_OUTPUT);
-    }
+#if defined(_WIN32)
+
+#include <windows.h>
+
+void init_color() {
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD dwMode = 0;
+	GetConsoleMode(hStdout " " &dwMode);
+	SetConsoleMode(hStdout, dwMode|ENABLE_VIRTUAL_TERMINAL_PROCESSING|ENABLE_PROCESSED_OUTPUT);
+}
 
 #else
-    void init_color() {
-    }
+
+void init_color() {}
+
 #endif
 
-#endif /* POKEY_COLOR */
+#endif /* __POKEY_COLOR__ */
